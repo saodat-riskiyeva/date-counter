0fc088fcad3c2e10e6f31a116d614342b15a8d9c
Saodat <saodat.riskiyeva@gmail.com>
2025-03-04 22:41:06 -0500

    upgrading the date counter

--- src/App.js
+++ src/App.js
@@ -11,87 +11,87 @@ export default function App() {
 
 function Counter() {
   const [step, setStep] = useState(0);
-  function handleStepUp() {
-    setStep(step + 1);
-  }
-
-  function handleStepDown() {
-    setStep(step - 1);
-  }
-
-  return (
-    <div>
-      <button onClick={handleStepDown}>-</button>
-      <span>Step: {step} </span>
-      <button onClick={handleStepUp}>+</button>
-      <CounterDate step={step} />
-    </div>
-  );
-}
-
-function CounterDate({ step }) {
   const [count, setCount] = useState(step);
 
-  function handleCountUp() {
-    setCount(count + step);
-  }
-
-  function handleCountDown() {
-    setCount(count - step);
-  }
-  return (
-    <div>
-      <button onClick={handleCountDown}>-</button>
-      <span>Count: {count} </span>
-      <button onClick={handleCountUp}>+</button>
-      <Calculator count={count} />
-    </div>
-  );
-}
-
-function Calculator({ count }) {
   const futureDate = new Date();
   futureDate.setDate(futureDate.getDate() + count);
-  // const months = [
-  //   "January",
-  //   "February",
-  //   "March",
-  //   "April",
-  //   "May",
-  //   "June",
-  //   "July",
-  //   "August",
-  //   "September",
-  //   "October",
-  //   "November",
-  //   "December",
-  // ];
 
-  // const days = [
-  //   "Sunday",
-  //   "Monday",
-  //   "Tuesday",
-  //   "Wednesday",
-  //   "Thursday",
-  //   "Friday",
-  //   "Saturday",
-  // ];
-
-  // const month = months[futureDate.getMonth()];
-  // const day = days[futureDate.getDay()];
-
-  // const newDate = `${day}, ${month} ${futureDate.getDate()} ${futureDate.getFullYear()}`;
-
+  function handleReset() {
+    setCount(0);
+    setStep(0);
+  }
   return (
     <div>
-      <span>
-        {count === 0
-          ? "Today is "
-          : count > 0
-          ? `${count} days from today is `
-          : `${Math.abs(count)} days ago was `}
-      </span>
-      <span> {futureDate.toDateString()} </span>
+      <div>
+        <input
+          type="range"
+          min="0"
+          max="10"
+          value={step}
+          onChange={(e) => setStep(Number(e.target.value))}
+        />
+        <span> {step} </span>
+      </div>
+      <div>
+        <button onClick={() => setCount((c) => c - step)}>-</button>
+        <input
+          type="text"
+          value={count}
+          onChange={(e) => setCount(Number(e.target.value))}
+        />
+        <button onClick={() => setCount((c) => c + step)}>+</button>
+      </div>
+
+      <p>
+        <span>
+          {count === 0
+            ? "Today is "
+            : count > 0
+            ? `${count} days from today is `
+            : `${Math.abs(count)} days ago was `}
+        </span>
+        <span> {futureDate.toDateString()} </span>
+      </p>
+
+      {step > 0 || count > 0 ? (
+        <div>
+          <button onClick={handleReset}>Reset</button>
+        </div>
+      ) : (
+        ""
+      )}
     </div>
   );
 }
+
+// function Calculator({ count }) {
+//   // const months = [
+//   //   "January",
+//   //   "February",
+//   //   "March",
+//   //   "April",
+//   //   "May",
+//   //   "June",
+//   //   "July",
+//   //   "August",
+//   //   "September",
+//   //   "October",
+//   //   "November",
+//   //   "December",
+//   // ];
+
+//   // const days = [
+//   //   "Sunday",
+//   //   "Monday",
+//   //   "Tuesday",
+//   //   "Wednesday",
+//   //   "Thursday",
+//   //   "Friday",
+//   //   "Saturday",
+//   // ];
+
+//   // const month = months[futureDate.getMonth()];
+//   // const day = days[futureDate.getDay()];
+
+//   // const newDate = `${day}, ${month} ${futureDate.getDate()} ${futureDate.getFullYear()}`;
+// }
